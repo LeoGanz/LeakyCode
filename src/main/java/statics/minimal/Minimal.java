4011a4505e56a58da8adc17a7512f6c8071e0e11
leonard <vp3ebj7j@duck.com>
2024-10-21 16:42:42 +0200

    Test codeql: Add data to map

--- src/main/java/statics/minimal/Minimal.java
+++ src/main/java/statics/minimal/Minimal.java
@@ -5,8 +5,9 @@ import java.util.Map;
 
 public class Minimal {
   public static void main(String[] args) {
-    String s = "field_oauth_token";
+    String s = "password";
     Map<String, String> json = new HashMap<>();
+    json.put(s, "1234");
     String res = json.get(s);
     System.out.println(res);
   }
